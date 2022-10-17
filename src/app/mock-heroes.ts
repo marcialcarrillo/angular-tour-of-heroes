79e3320c486f54b320adb16acbee1e0531e1c77a
marcialcarrillo <marcialcarrillov@gmail.com>
2022-10-17 10:23:26 -0600

    feat: add list, selected css (pt2)

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Sargon' },
+  { id: 13, name: 'Hercules' },
+  { id: 14, name: 'Achilles' },
+  { id: 15, name: 'Alexander' },
+  { id: 16, name: 'Julius' },
+  { id: 17, name: 'Augustus' },
+  { id: 18, name: 'William' },
+  { id: 19, name: 'Canute' },
+  { id: 20, name: 'Naram-Sin' },
+];
